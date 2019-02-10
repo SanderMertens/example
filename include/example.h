0c7ff9628e98a26362bdf06448e67a321fd5c259
Sander Mertens <sander.mertens8@gmail.com>
2019-02-10 02:21:10 -0800

    Initial commit

---
+++ include/example.h
@@ -0,0 +1,16 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+/* This generated file contains includes for project dependencies */
+#include "bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <include/example.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
